5c9ed22bc183048dbe4b7159487198d480ddda02
AhmedA <alkhatib_ahmed1995@hotmail.com>
2021-11-24 22:14:47 -0500

    html updated, small updates to css and js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,14 @@
+// Global Variables
 // var button for search button
+var searchButton = document.querySelector("#searchBtn")
 // inputValue for input search box
+var inputValue = document.querySelector("search-box")
+
+// Variables for hidden containers
+var history = document.querySelector("#history")
+var searchDisplay = document.querySelector("#search-display")
+var forecast = document.querySelector("#forecast")
+
 
 fetch("https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=9023560067b2e7bbffd9b2fa7315fa2f")
     .then(response => response.json())
@@ -8,4 +17,8 @@ fetch("https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=9023560
     .catch(err => alert("Wrong city name!"))
 
 
-  // 9023560067b2e7bbffd9b2fa7315fa2f
+
+
+
+  // API Key: 9023560067b2e7bbffd9b2fa7315fa2f
+  
